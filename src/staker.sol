b2e379dbb65b2d5964886b42ea9e411930ff75cc
Rakshitsinghhh <singhrakshit2404@gmail.com>
2025-06-21 21:27:50 +0000

    added token payable

--- src/staker.sol
+++ src/staker.sol
@@ -1,75 +1,82 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+interface IReward {
+    function mint(address account, uint256 amount) external;
+}
 
-contract Contract{
+contract Contract {
+    IReward public rewardToken;
 
-    uint256 private _totalSupply;
-    mapping(address=>uint256) public stakedbalance;
-    mapping(address=>uint256) public unclaimedReward;
-    mapping(address=>uint256) public lastUpdated;
-
-
-
-
-    function stake(uint256 val) public payable{
-        require(val > 0 , "amount must be greather then 0");
-        require(msg.value == val, "Amount must be equal to msg.value");
-        if(!lastUpdated[msg.sender])
-        {
-            lastUpdated[msg.sender]=block.timestamp;
-        }
-        else{
-
-            uint currentReward = unclaimedReward[msg.sender];
-            uint updateTime = lastUpdated[msg.sender];
-            uint newReward = (block.timestamp - updateTime ) * stakedbalance[msg.sender]; 
-            unclaimedReward[msg.sender] += newReward;
-            lastUpdated[msg.sender]=block.timestamp; 
-
-        }
-        _totalSupply+=val;
-        stakedbalance[msg.sender]+=val;
+    constructor(address _rewardToken) {
+        rewardToken = IReward(_rewardToken);
     }
 
-    function unstake(uint val) public {
+    uint256 private _totalSupply;
+
+    mapping(address => uint256) public stakedbalance;
+    mapping(address => uint256) public unclaimedReward;
+    mapping(address => uint256) public lastUpdated;
+
+    // Stake ETH and start earning rewards
+    function stake(uint256 val) public payable {
+        require(val > 0, "Amount must be greater than 0");
+        require(msg.value == val, "Amount must equal msg.value");
+
+        // If already staked, update rewards
+        if (lastUpdated[msg.sender] != 0) {
+            uint newReward = (block.timestamp - lastUpdated[msg.sender]) * stakedbalance[msg.sender];
+            unclaimedReward[msg.sender] += newReward;
+        }
+
+        lastUpdated[msg.sender] = block.timestamp;
+
+        stakedbalance[msg.sender] += val;
+        _totalSupply += val;
+    }
+
+    // Unstake ETH and update rewards
+    function unstake(uint256 val) public {
         require(val <= stakedbalance[msg.sender], "Insufficient staked balance");
 
-        uint currentReward = unclaimedReward[msg.sender];
-        uint updateTime = lastUpdated[msg.sender];
-        uint newReward = (block.timestamp - updateTime ) * stakedbalance[msg.sender]; 
+        // Update reward before changing balances
+        uint newReward = (block.timestamp - lastUpdated[msg.sender]) * stakedbalance[msg.sender];
         unclaimedReward[msg.sender] += newReward;
-        lastUpdated[msg.sender]=block.timestamp;
-        
-        
-        _totalSupply -= val;
+        lastUpdated[msg.sender] = block.timestamp;
+
         stakedbalance[msg.sender] -= val;
+        _totalSupply -= val;
+
         payable(msg.sender).transfer(val);
     }
 
-    function getreward(address _address) public view returns (uint256){
-        uint currentReward = unclaimedReward[_address];
-        uint updateTime = lastUpdated[_address];
-        uint newReward = (block.timestamp - updateTime ) * stakedbalance[_address];
+    // View the total pending reward for a user
+    function getReward(address _user) public view returns (uint256) {
+        uint currentReward = unclaimedReward[_user];
+        uint newReward = (block.timestamp - lastUpdated[_user]) * stakedbalance[_user];
         return currentReward + newReward;
-
     }
 
-    function claimReward() public{
+    // Claim rewards via ERC20 mint
+    function claimReward() public {
         uint currentReward = unclaimedReward[msg.sender];
-        uint updateTime = lastUpdated[msg.sender];
-        uint newReward = (block.timestamp - updateTime ) * stakedbalance[msg.sender];
+        uint newReward = (block.timestamp - lastUpdated[msg.sender]) * stakedbalance[msg.sender];
+        uint totalReward = currentReward + newReward;
 
-        //transfer
-        payable(msg.sender).transfer(currentReward+newReward);
+        require(totalReward > 0, "No rewards to claim");
 
-        unclaimedReward[msg.sender]=0;
-        lastUpdated[msg.sender]=block.timestamp;
+        rewardToken.mint(msg.sender, totalReward);
+
+        unclaimedReward[msg.sender] = 0;
+        lastUpdated[msg.sender] = block.timestamp;
     }
 
-    function totalSupply() public view returns(uint){
+    // Get total staked supply
+    function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
+
+    // Accept ETH
+    receive() external payable {}
 }
